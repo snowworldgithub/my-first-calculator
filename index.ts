26fcbedd284f8e56de84879ee953199a4ea919ce
snowworldgithub <160354474+snowworldgithub@users.noreply.github.com>
2024-03-16 02:22:57 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,33 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  {
+    message: "enter first number",
+    type: "number",
+    name: "firstNumber",
+  },
+  {
+    message: "enter second number",
+    type: "number",
+    name: "secondNumber",
+  },
+  {
+    message: "select one of the operators to perform action",
+    type: "list",
+    name: "operator",
+    choices: ["addition", "subtraction", "multiplication", "division"],
+  },
+]);
+
+//conditional statements
+if (answer.operator === "addition") {
+  console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operator === "subtraction") {
+  console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operator === "multiplication") {
+  console.log(answer.firstNumber * answer.secondNumber);
+} else if (answer.operator === "division") {
+  console.log(answer.firstNumber / answer.secondNumber);
+} else {
+  console.log("please select valid operator");
+}
